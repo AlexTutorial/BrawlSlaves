6da2a5f15f3627727bf5c0c18f37f700574d075d
AlexTutorial <78077438+AlexTutorial@users.noreply.github.com>
2022-05-05 20:37:01 +0300

    Update brawlers.py

--- brawlers.py
+++ brawlers.py
@@ -7,3 +7,16 @@ all_brawlers_list = [
     "Van_Darkholm",
     "Artem"
 ]
+
+brawlers_data = {
+    "Van_Darkholm": {
+        "hp": 2800,
+        "dmg": 360,
+        "Sdmg": 800
+    },
+    "Artem": {
+        "hp": 2500,
+        "dmg": 400,
+        "Sdmg": 850
+    }
+}
