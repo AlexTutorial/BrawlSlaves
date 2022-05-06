d1c258c76e649a1eedadd248c4c6e97e8d5085cf
AlexTutorial <pihtoded36@gmail.com>
2022-05-06 09:20:56 +0300

    new commit

---
+++ brawlers.py
@@ -0,0 +1,29 @@
+all_brawlers = {
+    "Van_Darkholm": "Вооружен ДШК и гранатой",
+    "Artem": "МЫШ (КРОДЕТЬСЯ)",
+    "###hub.com": "git commit"
+}
+
+all_brawlers_list = [
+    "Van_Darkholm",
+    "Artem",
+    "###hub.com"
+]
+
+brawlers_data = {
+    "Van_Darkholm": {
+        "hp": 2800,
+        "dmg": 360,
+        "Sdmg": 800
+    },
+    "Artem": {
+        "hp": 2500,
+        "dmg": 400,
+        "Sdmg": 850
+    },
+    "###hub.com": {
+        "hp": 2000,
+        "dmg": 600,
+        "Sdmg": 1050
+    }
+}
---
+++ level.py
@@ -0,0 +1,5 @@
+isFirstBoot = False 
+name = 'red-krok'
+cups=0
+gems=99302
+brawlers=['Van_Darkholm', 'Artem', '###hub.com']
